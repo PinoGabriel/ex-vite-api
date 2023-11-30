42dc6818aa48994cf09cf15df51c7ee33b075d67
PinoGabriel <pinogabriel01@gmail.com>
2023-11-30 16:29:32 +0100

    feat: create single card component

--- src/components/AppBirraCard.vue
+++ src/components/AppBirraCard.vue
@@ -1,23 +1,52 @@
 <script>
 export default {
     name: "AppBirraCard",
-    props: ["info"]
+    props: ["card"]
 }
 </script>
 
 <template>
-   <section class="container">
-        <div>{{ info.id }}</div>
-
-   </section>
+    <div class="container">
+        <div class="beerMargin">
+            <div class="w-33">
+                <h2>La Nostra Birra</h2>
+                <h3 class="my"> {{ card.name }}</h3>
+            </div>
+            <div class="w-33">
+                <h2>Ci trovi qui</h2>
+                <p class="my">{{ card.city }}</p>
+            </div>
+            <div>
+                <h2>Nazione di provenienza</h2>
+                <p class="my">{{ card.country }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-
 .container {
     width: 1100px;
     margin: 0 auto;
-    display: flex;
 }
 
+.beerMargin {
+    margin: 20px;
+    border: 1px solid red;
+    display: flex;
+    justify-content: space-between;
+}
+
+.w-33 {
+    width: 33%;
+}
+
+
+h2 {
+    color: red;
+}
+
+.my {
+    margin: 10px 0;
+}
 </style>
